ebd1a4ef8730902d7d024c8bf852c92cde57f247
Sunny singh <sunnyk7rajput@gmail.com>
2026-05-04 12:25:26 +0530

    Stage 3: Implementing DrawerActivity navigation layout and close functionality.

--- app/src/main/java/com/k7sunny/nexv1/DrawerActivity.java
+++ app/src/main/java/com/k7sunny/nexv1/DrawerActivity.java
@@ -2,6 +2,8 @@ package com.k7sunny.nexv1;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DrawerActivity extends AppCompatActivity {
@@ -11,6 +13,13 @@ public class DrawerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
 
+        ImageButton btn_close = findViewById(R.id.btn_close);
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
